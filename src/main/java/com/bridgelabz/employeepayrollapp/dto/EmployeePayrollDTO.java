5901e01f03d8149ea5f2ffcf315d3e107639f65b
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-07-30 03:26:42 +0530

    Adding UC-1-To add validation to name field so Rest call can be validated.

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -2,8 +2,16 @@ package com.bridgelabz.employeepayrollapp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
+    @NotEmpty(message = "Employee name cannot be null")
     public String name;
+    @Min(value = 500, message = "min wage should be more than 500")
     public long salary;
 
     public EmployeePayrollDTO(String name, long salary) {
